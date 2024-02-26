05e9caf88c158a488eec7c576ab36e4aa1042fe2
robsonbucci <robsonbucci@hotmail.com>
2024-02-26 15:45:53 -0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
 import About from './pages/About';
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl">
+            <span className="text-slate-500 ">Relatório </span>
+            <span> | </span>
+            <span className="text-slate-700 ">Congregacional</span>
+          </h1>
+        </Link>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="text-slate-700 hover:underline">About</li>
+          </Link>
+          <Link to="/signin">
+            <li className="text-slate-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
