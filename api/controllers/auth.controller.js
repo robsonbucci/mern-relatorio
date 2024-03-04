fb5628e328a283893999696267dcb2a6e1865a24
robsonbucci <robsonbucci@hotmail.com>
2024-03-04 18:05:22 -0300

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,7 +14,23 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'Usuário criado com sucesso' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(new Error(errorHandler(404, 'Usuário não encontrado')));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(new Error(errorHandler(404, 'usuário ou senha inválidos')));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie('accress_token', token, { httpOnly: true }).status(200).json({ rest });
   } catch (error) {
     next(error);
   }
