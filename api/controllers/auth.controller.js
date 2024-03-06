24cc734863aec49faa59a02a5a07f85e8326876e
robsonbucci <robsonbucci@hotmail.com>
2024-03-06 16:52:01 -0300

    update the header and make the profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
-    res.cookie('accress_token', token, { httpOnly: true }).status(200).json({ rest });
+    res.cookie('accress_token', token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
@@ -42,7 +42,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      res.cookie('accress_token', token, { httpOnly: true }).status(200).json({ rest });
+      res.cookie('accress_token', token, { httpOnly: true }).status(200).json(rest);
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
